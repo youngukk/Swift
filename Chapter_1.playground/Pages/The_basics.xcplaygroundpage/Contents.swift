d33a12ff4715efdc46277e8b3b52d098448a2727
youngukk <dev.younguk@gmail.com>
2021-08-22 11:37:56 +0900

    update

--- Chapter_1.playground/Pages/The_basics.xcplaygroundpage/Contents.swift
+++ Chapter_1.playground/Pages/The_basics.xcplaygroundpage/Contents.swift
@@ -1,9 +1,17 @@
 //: [Previous](@previous)
 
+
 import Foundation
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
 
-var greeting = "Hello, playground"
+print(a+b)
 
-print(greeting)
+
+
+
+
 
 //: [Next](@next)
