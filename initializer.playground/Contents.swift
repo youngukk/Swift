1fbee93df55908fbabca614b85643413c76b30c6
youngukk <dev.younguk@gmail.com>
2022-01-05 23:49:54 +0900

    Update

--- initializer.playground/Contents.swift
+++ initializer.playground/Contents.swift
@@ -38,3 +38,37 @@ protocol SomeProtocl {
 	static func typeMethod()
 	func random() -> Double
 }
+
+struct FixedLengthRange {
+		var firstValue: Int
+		let length: Int
+}
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+// the range represents integer values 0, 1, and 2
+rangeOfThreeItems.firstValue = 6
+// the range now represents integer values 6, 7, and 8
+
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+// this range represents integer values 0, 1, 2, and 3
+rangeOfFourItems.firstValue = 6
+// this will report an error, even though firstValue is a variable property
+
+class DataImporter {
+		/*
+		DataImporter is a class to import data from an external file.
+		The class is assumed to take a nontrivial amount of time to initialize.
+		*/
+		var filename = "data.txt"
+		// the DataImporter class would provide data importing functionality here
+}
+
+class DataManager {
+		lazy var importer = DataImporter()
+		var data: [String] = []
+		// the DataManager class would provide data management functionality here
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+// the DataImporter instance for the importer property hasn't yet been created
